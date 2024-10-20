129beafb58616c764058aa0cdfded38df5b05d59
Kingslaye-varun <rahatgaonkarvarun@gmail.com>
2024-10-20 13:36:41 +0530

    Employee and Department table created

--- Experiment.sql
+++ Experiment.sql
@@ -1,1 +1,25 @@
-create table Customer
+create database COMPANY
+
+use COMPANY
+
+create table EMPLOYEE(
+fname char(20), 
+mname char(20), 
+lname char(20), 
+ssn int primary key, 
+bdate date, 
+addr varchar(50), 
+sex varchar(1) check(sex in ('M','F')),
+salary money,
+superssn int foreign key references EMPLOYEE,
+Dno int); 
+select *from EMPLOYEE;
+
+create table DEPARTMENT(
+Dname varchar(20),
+Dnumber int primary key,
+Mgr_ssn int,
+Mgr_start_date date);
+select *from DEPARTMENT;
+
+
