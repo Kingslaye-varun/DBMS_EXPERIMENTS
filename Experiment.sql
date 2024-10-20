6b89b038cdc3a44549f2cd0bb8f3fe4b45c6db7b
Kingslaye-varun <rahatgaonkarvarun@gmail.com>
2024-10-20 21:21:13 +0530

    All tables created

--- Experiment.sql
+++ Experiment.sql
@@ -22,4 +22,35 @@ Mgr_ssn int,
 Mgr_start_date date);
 select *from DEPARTMENT;
 
+alter table EMPLOYEE add foreign key(Dno) references DEPARTMENT;
+
+create table DEPT_LOCATIONS(
+Dnumber int foreign key references DEPARTMENT,
+Dlocation varchar(15) primary key);
+select *from DEPT_LOCATIONS;
+
+alter table DEPT_LOCATIONS alter column Dnumber int not null;
+
+create table PROJECT(
+Pname varchar(20),
+Pnumber int primary key,
+Plocation varchar(15),
+Dnum int foreign key references EMPLOYEE);
+
+select *from PROJECT;
+
+create table WORKS_ON(
+Essn int foreign key references EMPLOYEE,
+Pno int primary key foreign key references PROJECT,
+Hours float);
+select *from WORKS_ON;
+
+create table DEPENDENT(
+Essn int foreign key references EMPLOYEE,
+Dependent_name varchar(20),
+Sex varchar(1) check(Sex in ('M','F')),
+Bdate date,
+Relationship varchar(15),
+primary key(Essn,Dependent_name));
+select *from DEPENDENT;
 
